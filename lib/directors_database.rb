d5f2c365ff3fc8722d1b02abf9b5623452bc73ac
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-08 23:40:46 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -12,6 +12,7 @@ def pretty_print_nds(nds)
    movies= ["Close Encounters of the Third Kind","Close Encounters of the Third Kind", "the Extra-terrestrial list "  ]
    a_o_a= [[1,2,3],[456],[7,8,9]]
 end
+pp nds 
 
 
 
