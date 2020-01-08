b0af3a2abe8c679d5e8f870ff742b0bc011f31a1
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-08 23:37:32 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -9,17 +9,7 @@ def pretty_print_nds(nds)
   
   nil 
 
-names=["Jaws", "E.T","Schindler"]
-   movies= ["Close Encounters of the Third Kind","Close Encounters of the Third Kind", "the Extra-terrestrial list "  ]
-   a_o_a= [[1,2,3],[456],[7,8,9]]
-   
-  
-   
- end 
- pp 'names'
-  pp 'movies'
-  pp 'a_o_a'
-  
+
  
 
  
